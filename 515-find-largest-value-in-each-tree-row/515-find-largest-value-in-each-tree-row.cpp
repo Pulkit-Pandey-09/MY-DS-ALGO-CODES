a5416ac5cc96948fe5cca29bd67b4662f3d3b5f0
Pulkit Pandey <70333073+Pulkit-Pandey-09@users.noreply.github.com>
2022-05-18 03:49:13 +0530

    Time: 19 ms (44.10%), Space: 22.2 MB (69.15%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -10,47 +10,24 @@
  * };
  */
 class Solution {
-public:
-    
-   vector<vector<int>> levelOrderTraversal(TreeNode* root)
+public:   
+    vector<int> largestValues(TreeNode* root) 
     {
-        vector<vector<int>> ans;
+        vector<int> ans;
         if(!root) return ans;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty())
         {
             int s = q.size();
-            vector<int> temp;
+            int temp = INT_MIN;
             while(s--)
             {
                 TreeNode* currNode = q.front();
                 q.pop();
                 if(currNode->left) q.push(currNode->left);
                 if(currNode->right) q.push(currNode->right);
-                temp.push_back(currNode->val);
-            }
-            ans.push_back(temp);
-        }
-        return ans;
-    }
-    
-    vector<int> largestValues(TreeNode* root) 
-    {
-        /* levelOrderTraversal array
-            1 fill levelOrderTraversal array/vector
-                1.a height of tree. --- O(n2)
-                1.b using queue.    --- O(n) 
-            2. traverse levelOrderTraversal array/vector to fill my answer vector
-        */
-        vector<vector<int>> levelOrder = levelOrderTraversal(root);
-        vector<int> ans;
-        for(int i = 0; i < levelOrder.size(); i++)
-        {
-            int temp = INT_MIN;
-            for(int j = 0; j < levelOrder[i].size(); j++)
-            {
-                temp = max(temp, levelOrder[i][j]);
+                temp = max(temp, currNode->val);
             }
             ans.push_back(temp);
         }
