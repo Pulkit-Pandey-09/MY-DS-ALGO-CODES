8787627231c5c1f4ede4ec06fe304b5280db5521
Pulkit Pandey <70333073+Pulkit-Pandey-09@users.noreply.github.com>
2022-07-13 01:31:36 +0530

    Time: 57 ms (33.88%), Space: 13.5 MB (57.16%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,6 +1,6 @@
-class Solution {
+class Solution1 {
 public:
-    
+    // recursion + memo ( top down approach)
     int solve(vector<int>& coins, int n, int amt, int i, vector<vector<int>>& dp)
     {
         // base case
@@ -24,5 +24,47 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
         int ans = solve(coins, n, amount, 0, dp);
         return ans;
+    }
+};
+class Solution {
+public:
+    // bottom -up approach
+    int dp[301][5001];
+    
+    int tabulation(int w, vector<int>& wt, int n)
+    {
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= w; j++)
+            {
+                if(i == 0 || j == 0)
+                {
+                    dp[i][j] = (j == 0 ? 1 : 0);
+                }
+            }
+        }
+        dp[0][0] = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= w; j++)
+            {
+                int incl = 0;
+                int exl = 0;
+                if(wt[i-1] <= j)
+                {
+                    incl = dp[i][j-wt[i-1]];
+                }
+                exl = dp[i-1][j];
+                dp[i][j] = incl + exl;
+            }
+        }
+        
+        return dp[n][w];
+    }
+    
+    int change(int amount, vector<int>& coins) 
+    {
+        memset(dp, -1, sizeof(dp));
+        return tabulation(amount, coins, coins.size());    
     }
 };
