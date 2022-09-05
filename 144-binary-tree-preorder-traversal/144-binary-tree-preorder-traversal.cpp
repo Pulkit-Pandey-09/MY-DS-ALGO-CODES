a074e17b3fef68d5048b9a347b49c783157aa2a0
Pulkit Pandey <70333073+Pulkit-Pandey-09@users.noreply.github.com>
2022-09-05 08:59:20 +0530

    Time: 3 ms (60.45%), Space: 8.4 MB (41.81%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root)  // morris preorder traversal
+    {
+        TreeNode* curr = root;
+        TreeNode* prev = root;
+        vector<int> preorder;
+        while(curr)
+        {
+            if(!curr->left) // 1.there is no left tree  2.curr node is root node
+            {
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else // I want to come back to my root node in O(1) time. So ill add threaded edge from last-most right node to curr node .
+            {
+                prev = curr->left;
+                while(prev->right && prev->right != curr)
+                {
+                    prev = prev->right;
+                }
+                
+                if(!prev->right)
+                {
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                }
+                if(prev->right == curr) // threaded edge already added before - means this left tree has already been processed.
+                {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+        return preorder;
+    }
+};
